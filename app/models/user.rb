da4238bc54e30c0dde8c246b930dcbc331fa150b
kuniyuki kikuchi <kkikuchi@netprotections.co.jp>
2016-06-16 08:31:30 +0000

    kaidai1

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,13 @@ class User < ActiveRecord::Base
 
   has_many :ownerships , foreign_key: "user_id", dependent: :destroy
   has_many :items ,through: :ownerships
-
+  
+  # ユーザーのwant,haveアイテムの一覧
+  has_many :wants, class_name: "Want", foreign_key: "user_id", dependent: :destroy
+  has_many :want_items , through: :wants, source: :item
+  
+  has_many :haves, class_name: "Have", foreign_key: "user_id", dependent: :destroy
+  has_many :have_items , through: :haves, source: :item
 
   # 他のユーザーをフォローする
   def follow(other_user)
