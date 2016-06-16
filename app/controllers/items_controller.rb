f8862db15bee55511d4b1c47b77ffa79b221920a
kuniyuki kikuchi <kkikuchi@netprotections.co.jp>
2016-06-16 08:15:23 +0000

    kaidai1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,20 +3,30 @@ class ItemsController < ApplicationController
   before_action :set_item, only: [:show]
 
   def new
+# 検索中処理
     if params[:q]
       response = RakutenWebService::Ichiba::Item.search(
         keyword: params[:q],
         imageFlag: 1,
       )
+      p response.count
+      p response.first
+
+# データと一致するものをとってくる
       @items = response.first(20)
     end
+    
   end
 
   def show
+# 商品詳細を表示する動作
+　  
   end
 
   private
+  
   def set_item
     @item = Item.find(params[:id])
   end
+  
 end
